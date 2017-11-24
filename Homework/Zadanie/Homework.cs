3ce55e4701db6a5347705818b82259c1bfa8c561
Andrej Juhas <andrejjuhas@yahoo.com>
2017-11-24 21:33:25 +0100

    Homework okomentovany

--- Homework/Zadanie/Homework.cs
+++ Homework/Zadanie/Homework.cs
@@ -22,27 +22,43 @@
 //            try
 //            {
 //                FileStream sourceStream = File.Open(sourceFileName, FileMode.Open);
+//Chyba tu using. Alebo da sa pridat nakonci vo finaly bloku uvolnenie cez interface ako to microsoft pise tu https://docs.microsoft.com/cs-cz/dotnet/csharp/language-reference/keywords/using-statement
+//Niektori programatori veria ze using je nejaky magicky prikaz . Pravda je taka ze microsoft otvorene hovori ze aj tak sa vytvori pocas compile time namiesto toho finaly block a zavola sa interface.
+//Toto plati pre vsetky komentare typu "chyba tu using".
+
 //                var reader = new StreamReader(sourceStream);
+//                //chyba tu using
+
 //                string input = reader.ReadToEnd();
 //            }
 //            catch (Exception ex)
 //            {
 //                throw new Exception(ex.Message);
+//ak vytvorite jednu jedinu custom exception budete presne vediet kde v kode nastala.
+// custom excetions umoznuju napriklad aj automaticke logovanie, zobrazovanie vnutorneho stavu programu kedy exception nastala , posielanie cez service a podobne...
 //            }
 
 //            var xdoc = XDocument.Parse(input);
+//            //input premenna neexistuje 
 //            var doc = new Document
 //            {
 //                Title = xdoc.Root.Element("title").Value,
 //                Text = xdoc.Root.Element("text").Value
 //            };
+//            //stringy by mali byt definovane ako konstanta
 
 //            var serializedDoc = JsonConvert.SerializeObject(doc);
+// Nehandluje sa tu exception. Exception treba pridat pretoze je to serializacia, je mozne ze nieco v instancii doc neojde zoserializovat.
+
+
 
 //            var targetStream = File.Open(targetFileName, FileMode.Create, FileAccess.Write);
+//               // nepouziva sa tu using
 //            var sw = new StreamWriter(targetStream);
+//            //chyba tu using
 //            sw.Write(serializedDoc);
-
+//            // chyba tu flush
+// Nehandluje sa tu exception. Exception treba pridat pretoze su to io operacie  a operacie so streami.
 
 //        }
 //    }
